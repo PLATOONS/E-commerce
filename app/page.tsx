8ce5bc374fd57ed48e4a6f7ce299a0f9ab3b607f
gloween <mmarevalop.smmc@gmail.com>
2025-09-13 19:47:09 -0600

    fix?

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
 import Header from "@/components/Header";
-import DiscountSection from "@/components/DiscountSection"; // Import for "Discount Section"
+import DiscountSection from "@/components/DiscountSection";
 import SubcategoriesSection from "@/components/SubcategoriesSection";
 import NewsletterSection from "@/components/NewsletterSection";
 import BenefitSection from "@/components/BenefitSection";
 import ProductCarousel from "@/components/ProductCarousel";
-
+import ArticlesSection from "@/components/ArticlesSection"; // Import the ArticlesSection
 
 export default function Home() {
-  
   return (
     <div>
       <Header /> 
       <main>
         <SubcategoriesSection />
-        <ProductCarousel />
+        {/* <ProductCarousel /> */}
         <BenefitSection />
         <DiscountSection />
+        <ArticlesSection />
         <NewsletterSection />
       </main>
     </div>
