8b041f08a27beac454950a4e874ce3d9d10ba6dd
apoque96 <dereckcabrerang@gmail.com>
2025-10-08 12:06:46 -0600

    Component done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../Frontend/styles.css"; // Import Styles from the original mock-up
+import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
