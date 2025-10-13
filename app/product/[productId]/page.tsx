ba5e5766bed1bd215d7515cb241715fe8dd1bcfc
apoque96 <dereckcabrerang@gmail.com>
2025-10-13 12:28:04 -0600

    merge main

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,3 +1,20 @@
-export default function Product(){
-    return <div>product</div>
+import Header from "@/components/Header";
+import NewsletterSection from "@/components/NewsletterSection";
+import Footer from "@/components/Footer";
+import ReviewsSection from "@/components/ReviewSection";
+import PriceComponent from "@/components/PriceComponent";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main className="px-6 py-10 space-y-12">
+        <h1 className="text-3xl font-bold">Product Page</h1>
+        <PriceComponent />
+        <ReviewsSection/>
+        <NewsletterSection />
+        <Footer />
+      </main>
+    </div>
+  );
 }
---
+++ components/PriceComponent.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface PriceComponentProps {
+  price?: number;
+  discounted_amount?: number;
+}
+
+const PriceComponent: React.FC<PriceComponentProps> = ({ price = 0, discounted_amount = 0 }) => {
+  const discountedPrice = price - discounted_amount;
+  const formatAsCurrency = (amount: number) => {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(amount);
+  };
+
+  return (
+    <div className="flex items-center gap-x-3">
+      <span className="text-2xl font-bold text-gray-900">
+        {formatAsCurrency(discountedPrice)}
+      </span>
+      <span className="text-lg text-gray-400 line-through">
+        {formatAsCurrency(price)}
+      </span>
+    </div>
+  );
+};
+
+export default PriceComponent;
