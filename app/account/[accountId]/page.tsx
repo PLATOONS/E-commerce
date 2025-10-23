6192449bd8ec469322a6c4626a8426356db709a6
DavidLepe <d.alconerolepe@gmail.com>
2025-10-22 19:14:04 -0600

    feat(account): show OrderHistory on /account

--- app/account/[accountId]/page.tsx
+++ app/account/[accountId]/page.tsx
@@ -1,3 +1,15 @@
-export default function Account(){
-    return <div>account</div>
+// app/account/page.tsx
+import OrderHistory from "@/components/OrderHistory";
+
+export default function AccountPage() {
+  return (
+    <main className="max-w-5xl mx-auto px-4 py-8">
+      <h1 className="text-2xl font-semibold mb-6">My Account</h1>
+
+      {/* Orders section */}
+      <section className="mt-4">
+        <OrderHistory />
+      </section>
+    </main>
+  );
 }
